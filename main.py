0fe7e1c318b3bd2f72fc8f0d1c80d2ce6c0eedfb
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-11 13:37:06 +0700

    init

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from deep_translator import GoogleTranslator
 from fastapi import FastAPI,Form
-import uvicorn
-
+from mangum import Mangum
 
 app = FastAPI()
 
-@app.post("/transalte")
+handler = Mangum(app)
+
+@app.get("/")
+async def root():
+    return {"message":"welcome to translator api free"}
+
+@app.post("/transaltor")
 def translate(text:str = Form(...)):
     translated = GoogleTranslator(source='en', target='km').translate(text)
-    return {"message":translated}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host= "192.168.12.161", port=8000)
+    return {"translated":translated}
