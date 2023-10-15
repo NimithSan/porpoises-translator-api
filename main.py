6153b7ff18f03fe1b6751c90be7706b45fa2ce0a
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-10-15 08:13:44 +0700

    front-end

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 from deep_translator import GoogleTranslator
-from fastapi import FastAPI,Form
+
 
 app = FastAPI()
 
+# Define the directory where your static files are located
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Create an instance of Jinja2Templates for rendering HTML templates
+templates = Jinja2Templates(directory="templates")  # Create a "templates" directory for your HTML templates
+
 @app.get("/")
-async def root():
-    return {"message":"welcome to translator api free"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/api/en-kh")
 def translate(text:str = Form(...)):
     translated = GoogleTranslator(source='en', target='km').translate(text)
     return {"translated":translated}
 
+#max translate khmer to eng is 1863 characters.
 @app.post("/api/kh-en")
 def translate(text:str = Form(...)):
     translated = GoogleTranslator(source='km', target='en').translate(text)
     return {"translated":translated}
 
-print("test brother")
+if __name__ == "__main__":
+    uvicorn.run(app, host="192.168.1.22", port=8000)
