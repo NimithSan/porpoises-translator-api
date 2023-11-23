4c8eb72e2d5d593063f4a14c5566db3cfe0da2ea
DemonLord <120779070+NimithSan@users.noreply.github.com>
2023-11-23 16:17:07 +0700

    done

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Porpoises Translator
 
-### About
-> free traslate api using google translate check it out url :.
+## About
 
-### Preview
-[![front end image](/src/front_end.png)](https://porpoises-translator.vercel.app)
+Porpoises Translator is a free translation tool that utilizes the Google Translate API through the `deep_translator` library.
 
-### Swagger UI
+## Usage
 
-[![swagger image](/src/swagger_ui.png)]((https://porpoises-translator.vercel.app/docs))
+To use the Porpoises Translator, you need to provide the following parameters:
 
+- **text:** The content that needs translation.
+- **src_language:** (Optional) The source language to translate from. If not provided, the tool will automatically detect the source language.
+- **target_language:** The target language to translate the text into.
 
+## Preview
 
+<div style="border: 2px solid #ddd; padding: 5px;">
+  <a href="https://porpoises-translator.vercel.app">
+    <img src="/src/front_end.png" alt="Front End Image" style="width:100%;">
+  </a>
+</div>
+
+## Swagger UI
+
+<div style="border: 1px solid #ddd; padding: 5px;">
+  <a href="https://porpoises-translator.vercel.app/docs">
+    <img src="/src/swagger_ui.png" alt="Front End Image" style="width:100%;">
+  </a>
+</div>
--- main.py
+++ main.py
@@ -3,28 +3,30 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from deep_translator import GoogleTranslator
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/src", StaticFiles(directory="src"), name="src")
 
-
 # Create an instance of Jinja2Templates for rendering HTML templates
-templates = Jinja2Templates(directory="templates") 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post('/api/translate')
-def translate_text(text: str = Form(...),src_language:str = Form(...),target_language: str = Form(...)):
-    translator = GoogleTranslator(source=src_language, target=target_language)
+
+@app.post("/api/translate", tags=["API"])
+def translate_text(
+    text: str = Form(...),
+    src_language: str = Form(None),  # Making src_language optional
+    target_language: str = Form(...),
+):
+    if src_language:
+        translator = GoogleTranslator(source=src_language, target=target_language)
+    else:
+        translator = GoogleTranslator(source="auto", target=target_language)
+
     translated_text = translator.translate(text)
     return {"translated_text": translated_text}
-
-# @app.post('/api/translate')
-# def translate_text(text: str = Form(...), target_language: str = Form(...)):
-#     translator = GoogleTranslator(source='auto', target=target_language)
-#     translated_text = translator.translate(text)
-#     return {"translated_text": translated_text}
